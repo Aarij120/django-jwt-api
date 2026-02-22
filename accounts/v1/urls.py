99cd11e8bb05b51fcc8b85e716c48c205a6ba7db
Khobaib Aarij <Khobaibaarij832@gmail.com>
2026-02-22 14:33:02 +0430

    Initial commit with JWT authentication and API versioning

---
+++ accounts/v1/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import RegisterView, ProtectedView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('protected/', ProtectedView.as_view()),
+]
---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "You are authenticated!",
+            "user": request.user.username
+        })
+
+# Create your views here.
